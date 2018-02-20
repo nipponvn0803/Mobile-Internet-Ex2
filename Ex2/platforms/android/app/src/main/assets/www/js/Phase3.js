6eaebe4574b3ff22a7709ba6ed5cbad99f397ce1
Unknown <caoson.trinh@gmail.com>
2018-02-20 21:38:29 +0200

    Phase 3

---
+++ Ex2/platforms/android/app/src/main/assets/www/js/Phase3.js
@@ -0,0 +1,31 @@
+function initMap() {
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    center: {lat: 41.85, lng: -87.65}
+  });
+  directionsDisplay.setMap(map);
+  directionsDisplay.setPanel(document.getElementById('desc'));
+
+  var onChangeHandler = function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  document.getElementById('submit').addEventListener('click', onChangeHandler);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  directionsService.route({
+    origin: document.getElementById('start').value,
+    destination: document.getElementById('end').value,
+    travelMode: 'DRIVING'
+  }, function(response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+
+
+}
